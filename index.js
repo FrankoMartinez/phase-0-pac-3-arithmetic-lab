d89bbe2fdd81a5868bd40686e94d6d10c4930673
Francisco <frankmartinez4050@gmail.com>
2021-07-31 23:43:38 -0500

    Completed assignment

---
+++ index.js
@@ -0,0 +1,35 @@
+function add(a, b) {
+    return a + b;
+};
+
+function subtract(a, b) {
+    return a - b;
+};
+
+function multiply(a, b) {
+    return a * b;
+};
+
+function divide(a, b) {
+    return a / b;
+};
+
+function increment(n = 5) {
+    n++;
+    return n;
+};
+
+function decrement(n = 10) {
+    n--;
+    return n;
+};
+
+function makeInt(n) {
+    parseInt('n');
+    return parseInt(n, 10);
+};
+
+function preserveDecimal(n) {
+     parseFloat('n');
+     return parseFloat(n);
+};
